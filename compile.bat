3d23caf1a964d39e84c8625fcfb78e7966f5fae8
Zi0MIX <80110206+Zi0MIX@users.noreply.github.com>
2023-05-24 21:39:13 +0200

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -3,15 +3,13 @@ gsc-tool.exe comp t6 "b2op.gsc"
 gsc-tool.exe comp t6 "plugin_templates\b2op_plugin_characters.gsc"
 gsc-tool.exe comp t6 "plugin_templates\b2op_plugin_fridge.gsc"
 gsc-tool.exe comp t6 "plugin_templates\b2op_plugin_hud.gsc"
+gsc-tool.exe comp t6 "plugin_templates\b2op_plugin_splits.gsc"
+
 @rem Old compiler
 Compiler.exe "b2op.gsc"
-Compiler.exe "plugin_templates\b2op_plugin_characters.gsc"
-Compiler.exe "plugin_templates\b2op_plugin_fridge.gsc"
-Compiler.exe "plugin_templates\b2op_plugin_hud.gsc"
+
 @rem Network Frame fix
 gsc-tool.exe comp t6 "network_frame_fix.gsc"
+
 @rem Move files
-MOVE /y "b2op-compiled.gsc" "compiled\t6\_clientids.gsc"
-MOVE /y "b2op_plugin_characters-compiled.gsc" "compiled\t6\_clientids-plugin-characters.gsc"
-MOVE /y "b2op_plugin_fridge-compiled.gsc" "compiled\t6\_clientids-plugin-fridge.gsc"
-MOVE /y "b2op_plugin_hud-compiled.gsc" "compiled\t6\_clientids-plugin-hud.gsc"
+MOVE /y "b2op-compiled.gsc" "compiled\t6\maps\mp\gametypes\_clientids.gsc"
